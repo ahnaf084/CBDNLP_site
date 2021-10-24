aa143b12f9b896e9e9a92464140eaf138936d003
Ahnaf Hamid <mhamid161275@bscse.uiu.ac.bd>
2021-10-24 22:52:17 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ span2.textContent=value;
 
 // create a new user
 var createUser = (user) => {
-    axios.post('https://cbdnlp.xyz:8080/predict', JSON.stringify({content: user}),{
+    axios.post('https://cbdnlp.xyz:5000/predict', JSON.stringify({content: user}),{
   headers: {
     'content-type': 'application/json'
   }
